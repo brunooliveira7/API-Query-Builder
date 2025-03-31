f949ce3ea13d3130abd9a221025b46bb3336e13c
Bruno Oliveira <brunoroliveira7@gmail.com>
2025-03-31 19:58:30 -0300

    relationship between tables

---
+++ src/database/migrations/20250331172407_create-course-modules.ts
@@ -0,0 +1,14 @@
+import type { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable("courses_modules", (table) => {
+    table.increments("id").primary();
+    table.text("name").notNullable();
+    //coluna de chave estrangeira - relacionar com a tabela de cursos
+    table.integer("course_id").references("id").inTable("courses");
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable("courses_modules");
+}
