d8fcb8cffeb3f97c84e67ca8d892b216d36b0b1b
Bruno Oliveira <brunoroliveira7@gmail.com>
2025-03-31 07:49:15 -0300

    creates the database through migration

--- src/database/migrations/20250329001641_create-courses.ts
+++ src/database/migrations/20250329001641_create-courses.ts
@@ -1,10 +1,16 @@
 import type { Knex } from "knex";
 
-
 export async function up(knex: Knex): Promise<void> {
+  //cria a tabela com nome courses
+  await knex.schema.createTable("courses", (table) => {
+    //cria as colunas
+    table.increments("id").primary(); //auto incremento e chave primária
+    table.text("name").notNullable();
+    table.timestamp("created_at").defaultTo(knex.fn.now()); //data de criação
+  });
 }
-
 
 export async function down(knex: Knex): Promise<void> {
+  //deleta a tabela
+  await knex.schema.dropTable("courses");
 }
-
